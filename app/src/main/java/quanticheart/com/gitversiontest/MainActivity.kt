6e849d61a511fe1542602777cbbb36bf5b8586c6
John Alves <dev.jonny255d@gmail.com>
2019-06-28 12:05:54 -0300

    description added in main activity

--- app/src/main/java/quanticheart/com/gitversiontest/MainActivity.kt
+++ app/src/main/java/quanticheart/com/gitversiontest/MainActivity.kt
@@ -9,6 +9,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /**
+         * Simple example
+         *
+         * fist, create tag for your project in git with command:
+         *
+         * git tag x.x.x (this number equal number your project)
+         *
+         * after call git add ., git commit ...
+         *
+         * for finish, call
+         *
+         * git push --tag (for push and create tag in your project)
+         *
+         *  **
+         *  if the project already has a version and you are adding this library now,
+         *  you must create the tag from the version number of the current project
+         *
+         *  example **
+         *  current project version 1.5.3
+         *
+         *  git tag 1.5.4
+         */
 
     }
 }
